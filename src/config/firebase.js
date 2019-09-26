43836bd5d89506373fe0458e521fe283a4a8b329
Mansoor <hussainmansoor876@gmail.com>
2019-09-26 22:27:07 +0500

    Updated1

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,14 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyC8DRxbVU0vPb7mhJIhUhCQJHw9O6UDfeY",
-    authDomain: "venue-club.firebaseapp.com",
-    databaseURL: "https://venue-club.firebaseio.com",
-    projectId: "venue-club",
-    storageBucket: "",
-    messagingSenderId: "836378910974",
-    appId: "1:836378910974:web:08d0af2b92b6a7c6"
-  };
+  apiKey: "AIzaSyBBFrIK16ve8Jwu7SJFmw-c6y5vzHw70ig",
+  authDomain: "eventmanager-4c064.firebaseapp.com",
+  databaseURL: "https://eventmanager-4c064.firebaseio.com",
+  projectId: "eventmanager-4c064",
+  storageBucket: "eventmanager-4c064.appspot.com",
+  messagingSenderId: "849403212630",
+  appId: "1:849403212630:web:bc79a91dec2d50d5f49cf7"
+};
 
-  firebase.initializeApp(config);
-  export default firebase;
- 
+firebase.initializeApp(config);
+export default firebase;
